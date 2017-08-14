dc96c20b5a3abfa1d4c2f19aa2d861f2dcd1147a
Markovic Predrag <markovic.predrag.gs@gmail.com>
2017-08-14 23:34:56 +0200

    Linted syntax

--- binary_finder.js
+++ binary_finder.js
@@ -1,20 +1,22 @@
 const { spawnSync } = require('child_process');
-
 //  Note: This function is not parsing every single directory for any possible chrome version.
-//  Instead, it tests if an installed chrome instance is available through the native BASH-CLI.
-exports.findChromeBinary = function() {
-   return new Promise(function (resolve, reject) {
-       const options = ['chrome', 'google-chrome', 'chromium'];
-       const versionPattern = new RegExp(/Google Chrome\s+\d+\.\d+\.\d+/);
-       var child;
-       //  Trys every option in the options-array
-       //  and checks the pattern of the 'option' --version output
-       //  in order to verify the authenticity of the program
-       options.forEach(function(value, index, array) {
-           child = spawnSync(value, ['--version']);
-           if(!child.error && versionPattern.test(child.stdout))
-               resolve([value]);
-       });
-       reject('Couldn\'t find chrome on the system');
-   });
+//  Instead, it tests if an installed chrome instance is available over the native BASH.
+
+function BinaryFinder() {
+  this.findChromeBinary = function findChromeBinary() {
+    return new Promise((resolve, reject) => {
+      const possibleBinaryNames = ['chrome', 'google-chrome', 'chromium'];
+      const versionPattern = new RegExp(/Google Chrome\s+\d+\.\d+\.\d+/);
+      let command;
+      //  Trys every binary-name in the array
+      //  and checks the pattern of the [binary-name] --version output
+      //  in order to verify the authenticity of the binary
+      possibleBinaryNames.forEach((binary) => {
+        command = spawnSync(binary, ['--version']);
+        if (!command.error && versionPattern.test(command.stdout)) resolve([binary]);
+      });
+      reject('Couldn\'t find chrome on the system');
+    });
+  };
 }
+module.exports = BinaryFinder;
---
+++ fileops.js
@@ -0,0 +1,55 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const os = require('os');
+
+function FileOps() {
+  this.createHtmlFile = function createHtmlFile(content, filename) {
+    const htmlFilePath = `${os.tmpdir()}${'/'}${filename}${'.html'}`;
+    fs.writeFile(htmlFilePath, content);
+    return htmlFilePath;
+  };
+
+  this.removeHtmlFile = function removeHtmlFile(htmlfile) {
+    return new Promise((resolve, reject) => {
+      exec(`rm ${htmlfile}`, (err, stdout) => {
+        if (err) reject(err);
+        resolve(stdout);
+      });
+    });
+  };
+
+  this.createPdfReadStream = function createPdfReadStream(pdfFilePath) {
+    return new Promise((resolve) => {
+      // Run interval (1sec) to check if the .pdf file is created
+      const interval = setInterval(() => {
+        if (fs.existsSync(pdfFilePath)) {
+          const readstream = fs.createReadStream(pdfFilePath);
+          resolve(readstream);
+          clearInterval(interval);
+        }
+      }, 1000);
+    });
+  };
+
+  this.createPdfFile = function createPdfFile(chromeCommand) {
+    return new Promise((resolve, reject) => {
+      exec(chromeCommand.join(' '), (err) => {
+        if (err) reject(err);
+        resolve('Done');
+      });
+    });
+  };
+
+  this.printHTMLtoPdf = function printHTMLtoPdf(command, pdfFilePath, htmlFilePath, removeflag) {
+    return new Promise((resolve, reject) => {
+      this.createPdfFile(command).then(() => {
+        this.createPdfReadStream(pdfFilePath).then((readstream) => {
+          if (removeflag) this.removeHtmlFile(htmlFilePath);
+          resolve(readstream);
+        }).catch((createError) => { reject(createError); });
+      }).catch((readStreamError) => { reject(readStreamError); });
+    });
+  };
+}
+
+module.exports = FileOps;
