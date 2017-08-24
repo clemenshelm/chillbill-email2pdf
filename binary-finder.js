9dee6c0cc6633c8342059820ff48eb10619315dc
Aleks <markovic.aleks@outlook.de>
2017-08-24 18:09:41 +0200

    small changes

---
+++ binary-finder.js
@@ -0,0 +1,22 @@
+const commands = require('child_process');
+//  Note: This function is not parsing every single directory for any possible chrome version.
+//  Instead, it tests if an installed chrome instance is available over the native BASH.
+
+function BinaryFinder() {
+  this.findChromeBinary = function findChromeBinary() {
+    return new Promise((resolve, reject) => {
+      const possibleBinaryNames = ['chrome', 'google-chrome', 'chromium'];
+      const versionPattern = new RegExp(/Google Chrome\s+\d+\.\d+\.\d+/);
+      var command;
+      //  Trys every binary-name in the array
+      //  and checks the pattern of the [binary-name] --version output
+      //  in order to verify the authenticity of the binary
+      possibleBinaryNames.forEach((binary) => {
+        command = commands.spawnSync(binary, ['--version']);
+        if (!command.error && versionPattern.test(command.stdout)) resolve([binary]);
+      });
+      reject('Couldn\'t find chrome on the system');
+    });
+  };
+}
+module.exports = BinaryFinder;
