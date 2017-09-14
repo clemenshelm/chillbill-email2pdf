22501702923bfd5b27bb2be3eaa18fb924bd4404
Aleks <markovic.aleks@outlook.de>
2017-09-14 11:29:00 +0200

    added new binary

--- binary-finder.js
+++ binary-finder.js
@@ -7,18 +7,23 @@ const commands = require('child_process');
 function BinaryFinder() {
   this.findChromeBinary = function findChromeBinary() {
     return new Promise((resolve, reject) => {
-      const possibleBinaryNames = ['chrome', 'google-chrome', 'chromium'];
+      const possibleBinaryNames = ['chrome', 'google-chrome', 'chromium', "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"];
       const versionPattern = new RegExp(/Google Chrome\s+\d+\.\d+\.\d+/);
       let command;
       //  Trys every binary-name in the array
       //  and checks the pattern of the [binary-name] --version output
       //  in order to verify the authenticity of the binary
+      // replace needed for osx path
       possibleBinaryNames.forEach((binary) => {
         command = commands.spawnSync(binary, ['--version']);
-        if (!command.error && versionPattern.test(command.stdout)) resolve([binary]);
+        if (!command.error && versionPattern.test(command.stdout)) {
+          resolve([binary.replace(/ /g, '\\ ')]);
+        }
       });
+
       reject('Couldn\'t find chrome on the system');
     });
   };
 }
+
 module.exports = BinaryFinder;
